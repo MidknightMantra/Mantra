f5d2bd1b8bbca97e942b0cabcc151aa5af6dd512
Midknight Mantra <233432686+MidknightMantra@users.noreply.github.com>
2026-01-26 21:54:22 +0300

    Update config.js for environment variable support
    Refactor session ID and prefix logic to use environment variables.

--- config.js
+++ config.js
@@ -1,15 +1,21 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
+// Owner Settings
 global.owner = ['254700000000'] 
 global.packname = 'Mantra'
 global.author = 'MidknightMantra'
 global.sessionName = 'Mantra_Session' 
-global.prefa = ['.', '!', '/'] 
 
-// PASTE YOUR SESSION ID BELOW (OR USE ENV VAR)
-global.sessionId = process.env.SESSION_ID || '' 
+// Prefix Logic (Environment Variable > Default)
+// In Railway, set PREFIX to: .,#,!
+global.prefa = process.env.PREFIX ? process.env.PREFIX.split(',') : ['.', '!', '/']
 
+// Session ID Logic (Strictly Environment Variable)
+// You MUST set SESSION_ID in Railway/Heroku Variables
+global.sessionId = process.env.SESSION_ID
+
+// Watch for file changes
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
     fs.unwatchFile(file)
