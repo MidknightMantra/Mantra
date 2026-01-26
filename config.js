bba8a3049dbf49503d4ba636a7400925eb121cf5
Midknight Mantra <233432686+MidknightMantra@users.noreply.github.com>
2026-01-26 21:13:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,17 @@
-module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "Mantra~", // Put your Mantra~ ID here
-    PREFIX: ".",
-    OWNER_NUM: "254700000000", // Your number
-    READ_MESSAGE: false,
-    MODE: 'public'
-};
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.owner = ['254700000000'] // Put your number here
+global.packname = 'Mantra'
+global.author = 'MidknightMantra'
+global.sessionName = 'Mantra_Session' // Session folder
+global.prefa = ['.', '!', '/'] // Prefixes
+
+// Watch for file changes
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
