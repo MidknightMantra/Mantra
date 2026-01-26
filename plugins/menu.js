cf7e2a4eb1d27d7caba4fbba3ba5e79e46004b55
Midknight Mantra <233432686+MidknightMantra@users.noreply.github.com>
2026-01-26 21:49:50 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,7 +1,33 @@
+const fs = require('fs')
+const path = require('path')
+
 module.exports = {
-    cmd: ['menu', 'help'],
-    run: async ({ m }) => {
-        // Look how clean this is thanks to lib/simple.js!
-        await m.reply(`*Mantra v1.0*\n\nUser: ${m.sender}\nCommand: ${m.text}`);
+    cmd: 'menu',
+    run: async (conn, m, args) => {
+        const pluginFolder = path.join(__dirname, '../plugins')
+        let commands = []
+
+        // Read all files in plugins folder
+        fs.readdirSync(pluginFolder).forEach(file => {
+            if (file.endsWith('.js')) {
+                const plugin = require(path.join(pluginFolder, file))
+                if (plugin.cmd) commands.push(plugin.cmd)
+            }
+        })
+
+        let text = `*🔮 Mantra Bot 🔮*\n\n`
+        text += `*User:* @${m.sender.split('@')[0]}\n`
+        text += `*Commands:* ${commands.length}\n\n`
+        
+        commands.forEach(cmd => {
+            text += `⬡ ${global.prefa}${cmd}\n`
+        })
+
+        text += `\n*Lightweight. Dangerous. Effective.*`
+
+        await conn.sendMessage(m.chat, { 
+            text: text, 
+            mentions: [m.sender] 
+        }, { quoted: m })
     }
-};
+}
