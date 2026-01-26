dbd807260629ad8625076c4ea839399534b8a52c
Midknight Mantra <233432686+MidknightMantra@users.noreply.github.com>
2026-01-26 20:58:00 +0300

    Refactor log messages in start function
    Updated log messages for clarity during process management.

--- index.js
+++ index.js
@@ -3,23 +3,24 @@ const path = require('path');
 
 function start() {
     let args = [path.join(__dirname, 'main.js'), ...process.argv.slice(2)];
-    console.log('[MIDKNIGHT] Booting up...');
-    
     let p = spawn(process.argv[0], args, {
         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
     });
 
     p.on('message', data => {
         if (data === 'reset') {
-            console.log('[MIDKNIGHT] Restarting process...');
+            console.log('Restarting Mantra...');
             p.kill();
             start();
+            delete p;
         }
     });
 
     p.on('exit', code => {
-        console.error('[MIDKNIGHT] Exited with code:', code);
-        if (code !== 0) start(); // Auto-restart on crash
+        console.error('Exited with code:', code);
+        if (code !== 0) {
+            start(); // Auto-restart on crash
+        }
     });
 }
 
