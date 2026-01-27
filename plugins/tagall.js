7ca59aea2632357de4a59ff545df7ac10743fefb
Midknight Mantra <midknight@localhost.localdomain>
2026-01-27 11:12:17 -0500

    Fix: Update npm start to new entry point & remove legacy files

--- plugins/tagall.js
+++
@@ -1,9 +0,0 @@
-export default {
-    cmd: 'all',
-    run: async (conn, m, args, text) => {
-        if (!m.isGroup) return
-        const meta = await conn.groupMetadata(m.chat)
-        const mentions = meta.participants.map(p => p.id)
-        await conn.sendMessage(m.chat, { text: text || '.', mentions: mentions }, { quoted: m })
-    }
-}
