47a0b194fbd00c80472c3a2cda04f094f18cfc44
Midknight Mantra <233432686+MidknightMantra@users.noreply.github.com>
2026-01-27 13:39:49 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,16 @@
 import { watchFile, unwatchFile } from 'fs'
 import { fileURLToPath } from 'url'
 
-// --- GLOBAL SETTINGS ---
-global.owner = ['254732647560'] // Updated to match your Bot JID from logs
+global.owner = ['254732647560'] // Bot JID number
 global.packname = 'Mantra'
 global.author = 'MidknightMantra'
-global.sessionName = 'session'
-global.prefa = [',', '!'] // Your active prefixes
-global.sessionId = process.env.SESSION_ID || ""
+global.prefa = [',', '!'] 
 
-// --- BIG BOY FEATURES ---
 global.alwaysOnline = true      
 global.antiDelete = true        
 global.antiViewOnce = true      
 global.autoStatusRead = true    
-global.autoStatusSave = false    
 
-// --- RELOAD LOGIC ---
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
     unwatchFile(file)
